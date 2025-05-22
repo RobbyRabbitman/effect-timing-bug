2dfd9752c5ef7c31f7e5cf142aca0d3f07cf1d26
robert hasemann <robert.hasemann@hdi.de>
2025-05-22 10:23:00 +0200

    fix template issue

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,15 @@ import {
   input,
   linkedSignal,
   numberAttribute,
+  signal,
+  untracked,
   viewChild,
 } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 @Component({
   selector: 'example',
-  template: `Inside component: {{ values }}`,
+  template: `Inside component: {{ values() }}`,
 })
 export class Example {
   valueInput = input(0, {
@@ -21,30 +23,42 @@ export class Example {
   value = linkedSignal(this.valueInput);
 
   valuesEffect = effect(() =>
-    this.values.push([this.valueInput(), this.value()].join())
+    this.values.set([
+      ...untracked(this.values),
+      [this.valueInput(), this.value()].join(),
+    ])
   );
 
-  values: string[] = [];
+  values = signal<string[]>([]);
 }
 
 @Component({
   selector: 'app-root',
   imports: [Example],
   template: `
-    <ul>
-      <li>
-        Why is the effect not triggered when property binding syntax is used?
-      </li>
-      <li>
-        Is there a different timing in the first execution of the effect
-        depending on how a value is passed? Is there a semantic difference in
-        property binding vs passing a literal value in that aspect?
-      </li>
-      <li>When are effects executed for the first time in a component?</li>
-    </ul>
+    <p>
+      When is the exact timing of the first read of the effect?
+
+      <br />
+
+      Example 1: With a literal value for a input, we see 1 execution of the
+      effect with the input value of 1, the default value of 0 was not read.
+
+      <br />
+
+      Example 2: With a property binding for a input, we see 2 executions of the
+      effect with the input value of 2 AND the default value of 0 was read.
+
+      <br />
+
+      I guess the expected behavior is that the effect should only run once with
+      the input value of 2, since it was set from the very beginning, but
+      observation is that the effect resolved before the input has been set,
+      therefore we see 2 executions of the effect.
+    </p>
 
     <example #example1 value="1" />
-    <div>Outside Component: {{ values1 }}</div>
+    <div>Outside Component: {{ values1() }}</div>
     <div>
       Current Value via a view child reference:
       {{ [this.example1().valueInput(), this.example1().value()].join() }}
@@ -53,7 +67,7 @@ export class Example {
     <br />
 
     <example #example2 [value]="2" />
-    <div>Outside Component: {{ values2 }}</div>
+    <div>Outside Component: {{ values2() }}</div>
     <div>
       Current Value via a view child reference:
       {{ [this.example2().valueInput(), this.example2().value()].join() }}
@@ -64,19 +78,21 @@ export class App {
   example1 = viewChild.required('example1', { read: Example });
   example2 = viewChild.required('example2', { read: Example });
 
-  values1: string[] = [];
-  values2: string[] = [];
+  values1 = signal<string[]>([]);
+  values2 = signal<string[]>([]);
 
   values1Effect = effect(() =>
-    this.values1.push(
-      [this.example1().valueInput(), this.example1().value()].join()
-    )
+    this.values1.set([
+      ...untracked(this.values1),
+      [this.example1().valueInput(), this.example1().value()].join(),
+    ])
   );
 
   values2Effect = effect(() =>
-    this.values2.push(
-      [this.example2().valueInput(), this.example2().value()].join()
-    )
+    this.values2.set([
+      ...untracked(this.values2),
+      [this.example2().valueInput(), this.example2().value()].join(),
+    ])
   );
 }
 
